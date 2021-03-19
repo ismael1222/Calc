45c87302cfc0c820122189d823cdd30779787af5
MaxGremory <65925132+ismael1222@users.noreply.github.com>
2021-03-18 22:41:59 -0300

    Update main.js
    Update por si las dudas...

--- js/main.js
+++ js/main.js
@@ -18,7 +18,23 @@ var introp = document.getElementById("introp");
 var introa = document.getElementById("introa");
 var resultado = document.getElementById("resultado");
 if (color12 == 1){
-ao.style.color="#ffffff";
+var body = document.getElementById("body")
+var au = document.getElementById("au")
+var ae = document.getElementById("ae")
+var seis = document.getElementById("seis");
+var intro = document.getElementById("intro");
+var introq = document.getElementById("introq");
+var introw = document.getElementById("introw");
+var introe = document.getElementById("introe");
+var intror = document.getElementById("intror");
+var introt = document.getElementById("introt");
+var introy = document.getElementById("introy");
+var introi = document.getElementById("introi");
+var introo = document.getElementById("introo");
+var introp = document.getElementById("introp");
+var introa = document.getElementById("introa");
+var resultado = document.getElementById("resultado");
+body.style.color="#ffffff";
 au.style.color="#ffffff";
 ae.style.color="#ffffff";
 intro.style.color="#ffffff";
@@ -34,8 +50,25 @@ introo.style.color="#ffffff";
 introp.style.color="#ffffff";
 introa.style.color="#ffffff";
 resultado.style.border="#ffffff";
-document.body.style.backgroundColor="black";}else{
-ao.style.color="black"
+document.body.style.backgroundColor="black";}
+else{
+  var body = document.getElementById("body")
+var au = document.getElementById("au")
+var ae = document.getElementById("ae")
+var seis = document.getElementById("seis");
+var intro = document.getElementById("intro");
+var introq = document.getElementById("introq");
+var introw = document.getElementById("introw");
+var introe = document.getElementById("introe");
+var intror = document.getElementById("intror");
+var introt = document.getElementById("introt");
+var introy = document.getElementById("introy");
+var introi = document.getElementById("introi");
+var introo = document.getElementById("introo");
+var introp = document.getElementById("introp");
+var introa = document.getElementById("introa");
+var resultado = document.getElementById("resultado");
+body.style.color="black"
 au.style.color="black";
 ae.style.color="black";
 intro.style.color="black";
@@ -90,4 +123,60 @@ function regla(){
   var gxha = g * ha;
   var gxhadj = gxha / j;
   alert("El resultado es " +gxhadj);
-}
+};
+function def1(){
+  alert("Realizar una operación aritmética que consiste en reunir varias cantidades (generalmente numericas) en una sola ")
+};
+function def2(){
+  alert("Realizar una operación aritmética que consiste en sumar un número por si mismo tantas veces como indica otro número")
+};
+function def3(){
+  alert("Operación aritmética que consiste en quitar una cantidad de otra para averiguar la diferencia entre las dos")
+};
+function def4(){
+  alert("Realizar una operación aritmética que consiste en hallar las veces que una cantidad está contenida en otra ")
+};
+function def5(){
+  alert("La regla de tres es una forma de resolver problemas de proporcionalidad entre tres valores conocidos y una incógnita. En ella se establece una relación de linealidad, proporcionalidad, entre los valores ")
+};
+function aok(){
+  g.value="aok";
+};
+function Ejemploa(){
+  alert("Si 14 manzanas cuestan 5385 pesos, cuanto cuestan 44 manzanas? (el resultado, es la incognita \(la x\), el numero se alertara cuando le des al boton \"Resultado\"\) \(El resultado aproximado es de 16924.28 pesos\)")
+};
+function Restaurar(){
+  var ao = document.getElementById("ao")
+var au = document.getElementById("au")
+var ae = document.getElementById("ae")
+var seis = document.getElementById("seis");
+var intro = document.getElementById("intro");
+var introq = document.getElementById("introq");
+var introw = document.getElementById("introw");
+var introe = document.getElementById("introe");
+var intror = document.getElementById("intror");
+var introt = document.getElementById("introt");
+var introy = document.getElementById("introy");
+var introi = document.getElementById("introi");
+var introo = document.getElementById("introo");
+var introp = document.getElementById("introp");
+var introa = document.getElementById("introa");
+var resultado = document.getElementById("resultado");
+body.style.color="black"
+au.style.color="black";
+ae.style.color="black";
+intro.style.color="black";
+introq.style.color="black";
+introw.style.color="black";
+introe.style.color="black";
+intror.style.color="black";
+introppp.style.color="black";
+introt.style.color="black";
+introy.style.color="black";
+introi.style.color="black";
+introo.style.color="black";
+introp.style.color="black";
+introa.style.color="black";
+resultado.style.border="black";
+document.body.style.backgroundColor="#ffffff";
+};
